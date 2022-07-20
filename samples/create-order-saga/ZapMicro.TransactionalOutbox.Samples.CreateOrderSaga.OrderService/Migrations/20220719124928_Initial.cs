467c890196a80e4eea4e61b79a4bda698d3b1b4f
De Francesco, Daniele <daniele11.defrancesco@nttdata.com>
2022-07-20 12:55:59 +0200

    Added payment service for create order saga sample

---
+++ samples/create-order-saga/ZapMicro.TransactionalOutbox.Samples.CreateOrderSaga.OrderService/Migrations/20220719124928_Initial.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZapMicro.TransactionalOutbox.Samples.CreateOrderSaga.OrderService.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutboxMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Payload = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderLines",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductQuantity = table.Column<long>(type: "bigint", nullable: false),
+                    ProductPrice = table.Column<double>(type: "float", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderLines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderLines_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Adjustments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OfferId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Total = table.Column<double>(type: "float", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    OrderLineId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adjustments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Adjustments_OrderLines_OrderLineId",
+                        column: x => x.OrderLineId,
+                        principalTable: "OrderLines",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Adjustments_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adjustments_OrderId",
+                table: "Adjustments",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adjustments_OrderLineId",
+                table: "Adjustments",
+                column: "OrderLineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_OrderId",
+                table: "OrderLines",
+                column: "OrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adjustments");
+
+            migrationBuilder.DropTable(
+                name: "OutboxMessages");
+
+            migrationBuilder.DropTable(
+                name: "OrderLines");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ samples/create-order-saga/ZapMicro.TransactionalOutbox.Samples.CreateOrderSaga.PaymentService/Program.cs
@@ -0,0 +1,74 @@
+using System;
+using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ZapMicro.TransactionalOutbox.Configurations;
+using ZapMicro.TransactionalOutbox.Samples.CreateOrderSaga.PaymentService.DbContexts;
+using ZapMicro.TransactionalOutbox.Samples.CreateOrderSaga.PaymentService.Handlers;
+using ZapMicro.TransactionalOutbox.Samples.CreateOrderSaga.PaymentService.OutboxMessages;
+using ZapMicro.TransactionalOutbox.Samples.CreateOrderSaga.PaymentService.Repositories;
+using ZapMicro.TransactionalOutbox.Samples.CreateOrderSaga.PaymentService.Services;
+using ZapMicro.TransactionalOutbox.Samples.CreateOrderSaga.Shared.Events;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+builder.Services.AddTransactionalOutbox<PaymentServiceDbContext>(configBuilder =>
+    configBuilder.ConfigureDequeueOutboxMessagesConfiguration(new DequeueOutboxMessagesConfiguration
+    {
+        EmptyQueueDelayInSeconds = 1
+    }).ConfigureOutboxMessageHandler<OnPaymentFailedOutboxMessageHandler, OnPaymentFailedOutboxMessage>()
+        .ConfigureOutboxMessageHandler<OnPaymentSucceededOutboxMessageHandler, OnPaymentSucceededOutboxMessage>());
+builder.Services.AddDbContext<PaymentServiceDbContext>(options => options.UseSqlServer(
+    Environment.GetEnvironmentVariable("PAYMENTS_DATABASE_CONNECTION_STRING") ?? string.Empty
+    ));
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddHostedService<EventProcessingService>();
+builder.Services.AddScoped<IEventHandlers, EventHandlers>();
+builder.Services.AddScoped<IEventHandler, OnOrderCreatedEventHandler>();
+builder.Services.AddHealthChecks();
+
+QueueClient CreateOrderQueueClientFactory() => new QueueClient(Environment.GetEnvironmentVariable("QUEUE_STORAGE_CONNECTION_STRING"), Environment.GetEnvironmentVariable("CREATE_ORDER_QUEUE_NAME"));
+
+QueueClient PaymentCompletedQueueClientFactory() => new QueueClient(Environment.GetEnvironmentVariable("QUEUE_STORAGE_CONNECTION_STRING"), Environment.GetEnvironmentVariable("PAYMENT_COMPLETED_QUEUE_NAME"));
+
+builder.Services.AddScoped(sp => new EventProcessingService(sp, CreateOrderQueueClientFactory));
+builder.Services.AddScoped(sp => PaymentCompletedQueueClientFactory());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHealthChecks("/healthcheck");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PaymentServiceDbContext>();
+    db.Database.Migrate();
+    var queueClient1 = CreateOrderQueueClientFactory();
+    await queueClient1.CreateIfNotExistsAsync();
+    var queueClient2 = PaymentCompletedQueueClientFactory();
+    await queueClient2.CreateIfNotExistsAsync();
+}
+
+
+app.Run();
